463498cc178b3f87cbdf6f5480c0cbfb82c30dfe
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-20 06:37:39 -0300

    Create ex008.js

---
+++ exercicios/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { // Condição simples
+    console.log('Vocé ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança.')
