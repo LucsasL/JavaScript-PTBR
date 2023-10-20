9e3d3177d1c7af0650174b1cc372cc53fd772284
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-20 19:38:39 -0300

    Completando a marcação desafio 2

--- scripts/desafio2.js
+++ scripts/desafio2.js
@@ -1,9 +1,11 @@
-var idade = 50
+var txtval = window.document.querySelector('input#idanoNasc')
+var idade = Number(txtval.value)
+var res = 'teste'
 
 if (idade < 20) {
-
+    res.innerHTML = `<p>Detectamos </p>`
 } else if (idade < 50) {
 
 } else {
-    
+
 }
