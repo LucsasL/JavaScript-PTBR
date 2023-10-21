12323c0faf93ae126d84eca50aeb383808a57a9c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-21 20:56:56 -0300

    Finalizando desafio 2

--- desafios/scripts/desafio2.js
+++ desafios/scripts/desafio2.js
@@ -6,26 +6,54 @@ function verificar() {
     var atual = agora.getFullYear()
     var res = document.querySelector('div#res')
 
-    if (anoNasc == 0 || anoNasc > atual) {
+    if (anoNasc == 0 || anoNasc > atual || anoNasc < 1918) {
         window.alert('[ERRO] Verifique os dados e tente novamente.')
 
     } else {
         var txtsexo = window.document.getElementsByName('sexo')
-        var idade = agora - Number(atual.value)
+        var idade = Number(atual - anoNasc)
 
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if (txtsexo[0].checked) {
             genero = 'Homem'
 
+            if (idade >= 0 && idade <= 10) { // Criança
+                img.setAttribute('src', '../../imagens/crianca-m.png')
+
+            } else if (idade <= 20) { // Jovem
+                img.setAttribute('src', '../../imagens/jovem-m.png')
+
+            } else if (idade < 50) { // Adulto
+                img.setAttribute('src', '../../imagens/adulto-m.png')
+
+            } else { // Idoso
+                img.setAttribute('src', '../../imagens/idoso-m.png')
+
+            }
+
         } else {
             genero = 'Mulher'
 
+            if (idade >= 0 && idade <= 10) { // Criança
+                img.setAttribute('src', '../../imagens/crianca-f.png')
+
+            } else if (idade <= 20) { // Jovem
+                img.setAttribute('src', '../../imagens/jovem-f.png')
+
+            } else if (idade < 50) { // Adulto
+                img.setAttribute('src', '../../imagens/adulta-f.png')
+
+            } else { // Idoso
+                img.setAttribute('src', '../../imagens/idosa-f.png')
+
+            }
         }
+
+        var res = document.getElementById('res')
         res.innerHTML = `<p>Detectamos um ${genero} com ${idade} anos.</p>`
+        res.appendChild(img)
     }
-
-    var res = document.getElementById('res')
-
-    var idade = Number(atual - anoNasc)
 }
