a13003494f5578fc055de08cd28cdb3568311373
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-21 09:00:13 -0300

    Commit

--- desafios/scripts/desafio2.js
+++ desafios/scripts/desafio2.js
@@ -1,11 +1,31 @@
-var txtval = window.document.querySelector('input#idanoNasc')
-var idade = Number(txtval.value)
-var res = 'teste'
+function verificar() {
+    var txtval = window.document.querySelector('input#idanoNasc')
+    var anoNasc = Number(txtval.value)
 
-if (idade < 20) {
-    res.innerHTML = `<p>Detectamos </p>`
-} else if (idade < 50) {
+    var agora = new Date()
+    var atual = agora.getFullYear()
+    var res = document.querySelector('div#res')
 
-} else {
+    if (anoNasc == 0 || anoNasc > atual) {
+        window.alert('[ERRO] Verifique os dados e tente novamente.')
 
+    } else {
+        var txtsexo = window.document.getElementsByName('sexo')
+        var idade = agora - Number(atual.value)
+
+        var genero = ''
+
+        if (txtsexo[0].checked) {
+            genero = 'Homem'
+
+        } else {
+            genero = 'Mulher'
+
+        }
+        res.innerHTML = `<p>Detectamos um ${genero} com ${idade} anos.</p>`
+    }
+
+    var res = document.getElementById('res')
+
+    var idade = Number(atual - anoNasc)
 }
