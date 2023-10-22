ace7b6779c3e4582718e351035e3762b967c4424
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-21 23:06:51 -0300

    Create ex015.js

---
+++ exercicios/ex015.js
@@ -0,0 +1,20 @@
+var c = 1
+
+console.log('Vai começar...')
+
+while (c <= 10) { // Laço de repetição While
+    console.log(`Passo ${c} com o While`)
+    c++
+}
+
+console.log('FIM!')
+
+console.log('--------------------------------------------')
+
+console.log('Vai começar...')
+
+for (var c = 1; c <= 10; c++) { // Laço de repetição For
+    console.log(`Passo ${c} com o For`)
+}
+
+console.log('FIM!')
