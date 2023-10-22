58f82d838653fe3c776d1e74a1b35ab4ce6463c7
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-22 07:14:39 -0300

    Create ex016.js

---
+++ exercicios/ex016.js
@@ -0,0 +1,10 @@
+var num = [5, 8, 2, 9, 3]
+
+num[5] = 4
+
+num.push(7)
+
+console.log(`Nosso vetor é: ${num}`)
+console.log('-------------------------------')
+console.log(`Ele tem ${num.length} posições.`)
+console.log(`Seu primeiro valor é: ${num[0]}`)
