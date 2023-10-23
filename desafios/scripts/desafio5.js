ea9ab1ec1318b929bd156f2aca998f93e855830f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-23 20:29:36 -0300

    Update desafio5.js

--- desafios/scripts/desafio5.js
+++ desafios/scripts/desafio5.js
@@ -1,17 +1,20 @@
 var numeros = []
 
+var media = 0
+
+var teste = 10
+
 function AdicionandoNum() {
     let num = document.querySelector('input#idnum').value
 
-    for (let c = 0 ; c <= teste ; c++) {
+    if (num.length == 0) {
+        window.alert(`[ERRO] Adicione um valor antes de continuar.`)
 
-        if (num.length == 0) {
-            window.alert(`[ERRO] Adicione um valor antes de continuar.`)
-
-        } else if (num > 100 || num <= 0) {
-            window.alert(`[ERRO] O valor adicionado não condiz com a proposta mencionada.`)
-            
-        } else {
+    } else if (num > 100 || num <= 0) {
+        window.alert(`[ERRO] O valor adicionado não condiz com a proposta mencionada.`)
+        
+    } else {
+        if (num != numeros) {
             numeros.push(num)
             let res = document.createElement('option')
             let optTxt = document.createTextNode(`Valor ${num} adicionado`)
@@ -31,6 +34,9 @@ function AdicionandoNum() {
             if (num < menor) {
                 menor = num
             }
+
+        } else {
+            window.alert('[ERRO] Valor repetido.')
         }
     }
 }
@@ -40,13 +46,13 @@ function Analisar() {
         window.alert('[ERRO] Adicione valores antes de finalizar.')
     }
 
-    media = media / numeros.length
+    media /= numeros.length
 
     let saida = document.querySelector('output#saida')
 
-    saida.innerHTML += `<p>Ao todo, Foram adicionados ${numeros.length} ao Array.</p>`
-    saida.innerHTML += `<p>O maior valor informado foi o ${maior}.</p>`
-    saida.innerHTML += `<p>O menor valor informado foi o ${menor}.</p>`
-    saida.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
-    saida.innerHTML += `<p>A média de todos os valores digitados é ${media}</p>`
+    saida.innerHTML += `<p>Ao todo, Foram adicionados ${numeros.length} valores ao Array.</p>`
+    saida.innerHTML = `<p>O maior valor informado foi o ${maior}.</p>`
+    saida.innerHTML = `<p>O menor valor informado foi o ${menor}.</p>`
+    saida.innerHTML = `<p>Somando todos os valores temos ${soma}.</p>`
+    saida.innerHTML = `<p>A média de todos os valores digitados é ${media}</p>`
 }
