b1c004bd36b0c0502580aad33560d02b6121a7e0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-24 07:12:47 -0300

    Desafio (quase) pronto

--- desafios/scripts/desafio5.js
+++ desafios/scripts/desafio5.js
@@ -1,44 +1,41 @@
-var numeros = []
+let saida = document.querySelector('output#saida')
+let numeros = []
+let media = 0
 
-var media = 0
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
 
-var teste = 10
+    } else {
+        return false
+        
+    }
+}
 
 function AdicionandoNum() {
-    let num = document.querySelector('input#idnum').value
+    let num = document.querySelector('input#idnum')
 
-    if (num.length == 0) {
+    if (num.value.length == 0) {
         window.alert(`[ERRO] Adicione um valor antes de continuar.`)
 
-    } else if (num > 100 || num <= 0) {
+    } else if (num.value > 100 || num.value <= 0) {
         window.alert(`[ERRO] O valor adicionado não condiz com a proposta mencionada.`)
         
+    } else if (!inLista(num.value, numeros)) {
+        numeros.push(num.value)
+
+        let mostra = document.querySelector('select#idvalores')
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        mostra.appendChild(item)
+        saida.innerHTML = ''
+
     } else {
-        if (num != numeros) {
-            numeros.push(num)
-            let res = document.createElement('option')
-            let optTxt = document.createTextNode(`Valor ${num} adicionado`)
-
-            media += num
-
-            if (c == 0) {
-                var maior = num
-                var menor = num
-
-            }
-
-            if (num > maior) {
-                maior = num
-            }
-
-            if (num < menor) {
-                menor = num
-            }
-
-        } else {
-            window.alert('[ERRO] Valor repetido.')
-        }
+        window.alert('[ERRO] Valor repetido.')
     }
+
+    num.value = ''
+    num.focus()
 }
 
 function Analisar() {
@@ -46,13 +43,26 @@ function Analisar() {
         window.alert('[ERRO] Adicione valores antes de finalizar.')
     }
 
-    media /= numeros.length
+    let maior = numeros[0]
+    let menor = numeros[0]
+    let soma = Number(0)
 
-    let saida = document.querySelector('output#saida')
+    for (let c in numeros) {
+        soma += Number(numeros[c])
 
+        if (numeros[c] > maior) {
+            maior = numeros[c]
+        }
+        if (numeros[c] < menor) {
+            menor = numeros[c]
+        }
+    }
+    media = soma / numeros.length
+
+    saida.innerHTML = ''
     saida.innerHTML += `<p>Ao todo, Foram adicionados ${numeros.length} valores ao Array.</p>`
-    saida.innerHTML = `<p>O maior valor informado foi o ${maior}.</p>`
-    saida.innerHTML = `<p>O menor valor informado foi o ${menor}.</p>`
-    saida.innerHTML = `<p>Somando todos os valores temos ${soma}.</p>`
-    saida.innerHTML = `<p>A média de todos os valores digitados é ${media}</p>`
+    saida.innerHTML += `<p>O maior valor informado foi o ${maior}.</p>`
+    saida.innerHTML += `<p>O menor valor informado foi o ${menor}.</p>`
+    saida.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+    saida.innerHTML += `<p>A média de todos os valores digitados é ${media}</p>`
 }
