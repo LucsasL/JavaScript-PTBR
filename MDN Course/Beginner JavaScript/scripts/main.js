225661c9c5e15e66cd50b8c57d5df261e447523e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-12-02 13:53:31 -0300

    New course from MDN

---
+++ MDN Course/Beginner JavaScript/scripts/main.js
@@ -0,0 +1,10 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/ESTUDANDO PACAS.png")
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png")
+    }
+}
