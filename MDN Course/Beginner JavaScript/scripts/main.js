131f7ee5db5804b5d1d2a3fd38a33cb1b7d4a389
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-12-02 14:38:22 -0300

    First JavaScript concepts in MDN

--- MDN Course/Beginner JavaScript/scripts/main.js
+++ MDN Course/Beginner JavaScript/scripts/main.js
@@ -1,10 +1,39 @@
 const myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/firefox-icon.png") {
-        myImage.setAttribute("src", "images/ESTUDANDO PACAS.png")
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/ESTUDANDO PACAS.png');
+
     } else {
-        myImage.setAttribute("src", "images/firefox-icon.png")
+      myImage.setAttribute ('src','images/firefox-icon.png');
+
+    }
+}
+  
+myButton.onclick = () => {
+    setUserName();
+}
+
+if (!localStorage.getItems("name")) {
+    setUserName();
+
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.innerHTML = `Mozilla is cool, ${storedName}`;
+}
+
+function setUserName() {
+    let myName = prompt("Please, enter your name below: ");
+
+    if (!myName) {
+        setUserName();
+
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`
     }
 }
